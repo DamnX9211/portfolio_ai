3e882601437ea933395d24f7898fa725fd8fdd69
DamnX9211 <rohitkuumar1995@gmail.com>
2026-02-21 11:12:45 +0530

    initialize react + typescript using vite and configure tailwind with minimal black theme base

---
+++ frontend/src/App.tsx
@@ -0,0 +1,19 @@
+
+import './index.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="container mx-auto px-4">
+       <p className="text-2xl font-bold text-center mt-10">
+        Hello World! This is a portfolio website built with React and Tailwind CSS. It showcases my projects and skills in web development. Feel free to explore and check out my work!
+      </p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
